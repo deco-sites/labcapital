900dc4d3204f95fb9399ed1e0b503f24145b503e
Kesler <kesler.m.pereira@gmail.com>
2023-06-15 14:30:03 -0300

    feat: finished

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -18,15 +18,14 @@ export default function Header({ title }: Props) {
         <a
           href="https://github.com/peas/lab.capital"
           class="m-0 h-[40px] w-[fit-content] items-center justify-center flex no-underline text-[13px] text-[#fff] font-[700] rounded-[50px] shadow-[0px 1px 2px rgba(0,0,0,0.2)] hover:shadow-[0px 1px 2px rgba(0,0,0,0.4)] transition-[all 0.2s ease-in-out] border-2 border-black"
-          style={
-            {
-              "background": "-webkit-linear-gradient(top, rgba(40,40,40,0.3), rgba(35,35,35,0.3) 50%, rgba(10,10,10,0.3) 50%, rgba(0,0,0,0.3))",
-              "padding": "8px 18px",
-              "border-bottom": "2px solid rgba(0,0,0,0.7)",
-              "border-top": "2px solid #000",
-              "font-family": "Helvetica, Arial, sans-serif"
-            }
-          }
+          style={{
+            "background":
+              "-webkit-linear-gradient(top, rgba(40,40,40,0.3), rgba(35,35,35,0.3) 50%, rgba(10,10,10,0.3) 50%, rgba(0,0,0,0.3))",
+            "padding": "8px 18px",
+            "border-bottom": "2px solid rgba(0,0,0,0.7)",
+            "border-top": "2px solid #000",
+            "font-family": "Helvetica, Arial, sans-serif",
+          }}
         >
           <span class="bg-[url('https://lab.capital/assets/images/blacktocat.png')] w-[16px] h-[16px] inline-block opacity-[0.6] mt-[1px] mr-[8px] ">
           </span>
--- sections/WhoWeAre.tsx
+++ sections/WhoWeAre.tsx
@@ -1,38 +1,41 @@
 export interface Props {
+  title: string;
+  infoObject?: {
     title: string;
-    infoObject?: {
-        title: string;
-        text: string;
-        image: string;
-    }[];
-  }
-  
-  export default function WhoWeAre({ title, infoObject, ...rest }: Props) {
-    return (
-      <section className="pt-[20px]" {...rest}>
-        <section className="max-w-[1000px] w-[90%] mx-auto">
-          <h1 className="text-secondary text-[30px] font-normal mb-[20px]">
-            {title}
-          </h1>
-          <div className="flex flex-col">
-                {infoObject?.map((item, index) => {
-                    return (
-                        <div className="flex flex-col" tabIndex={index}>
-                            <h2 className="text-secondary text-[24px] font-normal mb-[15px]">
-                                {item.title}
-                            </h2>
-                            <code
-                            className="bg-black text-white p-[20px] mb-[30px] whitespace-nowrap overflow-x-auto md:whitespace-normal"
-                            dangerouslySetInnerHTML={{ __html: item.text }}
-                            >
-                            </code>
-                            <img src={item.image} alt="image" className={"w-[300px] mb-[30px]"}/>
-                        </div>
-                    )
-                })}
-            </div>
-        </section>
+    text: string;
+    image: string;
+  }[];
+}
+
+export default function WhoWeAre({ title, infoObject, ...rest }: Props) {
+  return (
+    <section className="pt-[20px]" {...rest}>
+      <section className="max-w-[1000px] w-[90%] mx-auto">
+        <h1 className="text-secondary text-[30px] font-normal mb-[20px]">
+          {title}
+        </h1>
+        <div className="flex flex-col">
+          {infoObject?.map((item, index) => {
+            return (
+              <div className="flex flex-col" tabIndex={index}>
+                <h2 className="text-secondary text-[24px] font-normal mb-[15px]">
+                  {item.title}
+                </h2>
+                <code
+                  className="bg-black text-white p-[20px] mb-[30px] whitespace-nowrap overflow-x-auto md:whitespace-normal"
+                  dangerouslySetInnerHTML={{ __html: item.text }}
+                >
+                </code>
+                <img
+                  src={item.image}
+                  alt="image"
+                  className={"w-[300px] mb-[30px]"}
+                />
+              </div>
+            );
+          })}
+        </div>
       </section>
-    );
-  }
-  
+    </section>
+  );
+}
